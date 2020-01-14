c5fa403ca7866a6a46e8bb19ddd824f780b7b77f
Alexander Wang <zxcvb22217@gmail.com>
2020-01-14 22:17:42 +0800

    feat: Add stream-related tools to util.ts
    * startStream(): Create a stream from a string.
    * endStream(): Collect all chunks of a stream and return a string.
    * decompressStream(): Decompress a stream.

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
 import { URL } from "url"
+import * as stream from "stream"
+import * as zlib from "zlib"
 
 /**
  * Infer which port to use.
@@ -25,4 +27,66 @@ function isProtocolSupported(url: URL) {
 
 
 
-export { inferPort, isProtocolSupported }
+/**
+ * Create a read stream from some data.
+ * @param data 
+ * @param _opt 
+ */
+function startStream(data: string, _opt?: { debug?: boolean }) {
+  const debug = _opt && _opt.debug
+  const buffer = Buffer.from(data)
+  let ptr = 0
+  return new stream.Readable({
+    read(size) {
+      if (debug) console.log(size, ptr)
+      if (ptr > buffer.length) {
+        this.push(null)
+      } else {
+        this.push(buffer.slice(ptr, ptr += size))
+      }
+    }
+  })
+}
+
+
+
+/**
+ * Collect all chunks and concat their content as one string.
+ * @param readStream 
+ */
+function endStream(readStream: NodeJS.ReadableStream) {
+  return new Promise<string>((resolve, reject) => {
+    let chunks: Buffer[] = []
+    readStream.on("data", (chunk) => { chunks.push(chunk) })
+    readStream.on("end", () => { resolve(Buffer.concat(chunks).toString()) })
+    readStream.on("error", (error) => { reject(error) })
+  })
+}
+
+
+
+/**
+ * Decompress a stream.
+ * @param iStream 
+ * @param encoding 
+ */
+function decompressStream(
+  iStream: NodeJS.ReadableStream,
+  encoding?: "br" | "gzip" | "deflate" | string
+): NodeJS.ReadableStream {
+  switch (encoding) {
+    case "br":
+      return iStream.pipe(zlib.createBrotliDecompress())
+    case "gzip":
+      return iStream.pipe(zlib.createGunzip())
+    case "deflate":
+      return iStream.pipe(zlib.createInflate())
+    default:
+      return iStream
+  }
+}
+
+export {
+  inferPort, isProtocolSupported,
+  startStream, endStream, decompressStream
+}
